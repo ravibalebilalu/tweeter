607b2498bd529d22c3194459136da67003529027
ravibalebilalu <81ravikiran@gmail.com>
2024-12-20 13:12:08 +0530

    add hyperlink

--- tweeterproject/tweetapp/serializers.py
+++ tweeterproject/tweetapp/serializers.py
@@ -2,18 +2,19 @@ from rest_framework import serializers
 from tweetapp.models import Tweet
 from django.contrib.auth.models import User
 
-class TweetSerializer(serializers.ModelSerializer):
+class TweetSerializer(serializers.HyperlinkedModelSerializer):
     creater = serializers.ReadOnlyField(source="creater.username")
+     
     class Meta:
         model = Tweet
-        fields = ["id","tweet_text","created_at","creater"]
+        fields = ["url", "id","tweet_text","created_at","creater"]
         
 
 
-class UserSerializer(serializers.ModelSerializer):
-    tweets = serializers.PrimaryKeyRelatedField(many=True,queryset=Tweet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    tweets = serializers.HyperlinkedRelatedField(many=True,view_name="tweet-detail",read_only=True)
 
     class Meta:
         model = User
-        fields = ["id","username","tweets"]
+        fields = ["url", "id","username","tweets"]
     
--- tweeterproject/tweetapp/urls.py
+++ tweeterproject/tweetapp/urls.py
@@ -4,10 +4,11 @@ from tweetapp import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("tweets/",views.TweetList.as_view()),
-    path("tweets/<int:pk>/",views.TweetDetail.as_view()),
-    path("users/",views.UserList.as_view()),
-    path("users/<int:pk>/",views.UserDetail.as_view()),
+    path("",views.api_root,name="tweets"),
+    path("tweets/",views.TweetList.as_view(),name="tweet-list"),
+    path("tweets/<int:pk>/",views.TweetDetail.as_view(),name="tweet-detail"),
+    path("users/",views.UserList.as_view(),name="user-list"),
+    path("users/<int:pk>/",views.UserDetail.as_view(),name="user-detail"),
 ]
 
 urlpatterns  = format_suffix_patterns(urlpatterns)
